296b7038be701489cbd9e32b1be5b840a28a202b
wrathematics <wrathematics@gmail.com>
2013-02-07 12:55:07 -0500

    02.07.13 merged new netcdf stuff

--- R/load_balance.r
+++ R/load_balance.r
@@ -1,6 +1,7 @@
 ### This file contains functions to load balance of data X.spmd.
 
-balance.info <- function(X.spmd, comm = .SPMD.CT$comm){
+balance.info <- function(X.spmd, comm = .SPMD.CT$comm,
+    spmd.major = .DEMO.CT$spmd.major){
   COMM.SIZE <- spmd.comm.size(comm)
   COMM.RANK <- spmd.comm.rank(comm)
 
@@ -8,7 +9,13 @@ balance.info <- function(X.spmd, comm = .SPMD.CT$comm){
     X.spmd <- as.matrix(X.spmd)
   }
 
-  N.spmd <- nrow(X.spmd)
+  if(spmd.major == 1){
+    N.spmd <- nrow(X.spmd)
+  } else if(spmd.major == 2){
+    N.spmd <- ncol(X.spmd)
+  } else{
+    stop("spmd.major = 1 or 2.")
+  }
   N.allspmd <- spmd.allgather.integer(as.integer(N.spmd), integer(COMM.SIZE),
                                       comm = comm)
   N <- sum(N.allspmd)
@@ -24,27 +31,43 @@ balance.info <- function(X.spmd, comm = .SPMD.CT$comm){
                           belong = rank.belong[rank.belong == COMM.RANK])
 
   list(send = send.info, recv = recv.info, N.allspmd = N.allspmd,
-       new.N.allspmd = new.N.allspmd)
+       new.N.allspmd = new.N.allspmd, spmd.major = spmd.major)
 } # End of balance.info()
 
 
-load.balance <- function(X.spmd, bal.info = NULL, comm = .SPMD.CT$comm){
+load.balance <- function(X.spmd, bal.info = NULL, comm = .SPMD.CT$comm,
+    spmd.major = .DEMO.CT$spmd.major){
   COMM.RANK <- spmd.comm.rank(comm)
   if(is.null(bal.info)){
-    bal.info <- balance.info(X.spmd)
+    bal.info <- balance.info(X.spmd, comm = comm, spmd.major = spmd.major)
   }
 
   if(!is.matrix(X.spmd)){
     X.spmd <- as.matrix(X.spmd)
   }
-  p <- ncol(X.spmd)
+  if(spmd.major == 1){
+    p <- ncol(X.spmd)
+  } else if(spmd.major == 2){
+    p <- nrow(X.spmd)
+  } else{
+    stop("spmd.major = 1 or 2.")
+  }
 
   send.to <- as.integer(unique(bal.info$send$belong))
   if(length(send.to) > 0){
-    for(i in send.to){
-      if(i != COMM.RANK){
-        tmp <- matrix(X.spmd[bal.info$send$belong == i,], ncol = p)
-        isend(tmp, rank.dest = i, tag = COMM.RANK, comm = comm)
+    if(spmd.major == 1){
+      for(i in send.to){
+        if(i != COMM.RANK){
+          tmp <- matrix(X.spmd[bal.info$send$belong == i,], ncol = p)
+          isend(tmp, rank.dest = i, tag = COMM.RANK, comm = comm)
+        }
+      }
+    } else{
+      for(i in send.to){
+        if(i != COMM.RANK){
+          tmp <- matrix(X.spmd[, bal.info$send$belong == i], nrow = p)
+          isend(tmp, rank.dest = i, tag = COMM.RANK, comm = comm)
+        }
       }
     }
   }
@@ -52,24 +75,42 @@ load.balance <- function(X.spmd, bal.info = NULL, comm = .SPMD.CT$comm){
   recv.from <- as.integer(unique(bal.info$recv$org))
   if(length(recv.from) > 0){
     ret <- NULL
-    for(i in recv.from){
-      if(i != COMM.RANK){
-        tmp <- recv(rank.source = i, tag = i, comm = comm)
-      } else{
-        tmp <- matrix(X.spmd[bal.info$send$belong == i,], ncol = p)
+    if(spmd.major == 1){
+      for(i in recv.from){
+        if(i != COMM.RANK){
+          tmp <- recv(rank.source = i, tag = i, comm = comm)
+          dim(tmp) <- c(length(tmp) / p, p)
+        } else{
+          tmp <- matrix(X.spmd[bal.info$send$belong == i,], ncol = p)
+        }
+        ret <- base:::rbind(ret, tmp)
       }
-      ret <- base:::rbind(ret, tmp)
+    } else{
+      for(i in recv.from){
+        if(i != COMM.RANK){
+          tmp <- recv(rank.source = i, tag = i, comm = comm)
+          dim(tmp) <- c(p, length(tmp) / p)
+        } else{
+          tmp <- matrix(X.spmd[, bal.info$send$belong == i], nrow = p)
+        }
+        ret <- base:::cbind(ret, tmp)
+      }
     }
   } else{
     ret <- X.spmd
   }
 
   if(bal.info$new.N.allspmd[spmd.comm.rank(comm) + 1] == 0){
-    ret <- matrix(0, nrow = 0, ncol = p)
+    if(spmd.major == 1){
+      ret <- matrix(0, nrow = 0, ncol = p)
+    } else{
+      ret <- matrix(0, nrow = p, ncol = 0)
+    }
   }
 
   ret
 } # End of load.balance().
+
 
 unload.balance <- function(new.X.spmd, bal.info, comm = .SPMD.CT$comm){
   rev.bal.info <- list(send = data.frame(org = bal.info$recv$belong,
@@ -77,7 +118,8 @@ unload.balance <- function(new.X.spmd, bal.info, comm = .SPMD.CT$comm){
                        recv = data.frame(org = bal.info$send$belong,
                                          belong = bal.info$send$org),
                        N.allspmd = bal.info$new.N.allspmd,
-                       new.N.allspmd = bal.info$N.allspmd)
+                       new.N.allspmd = bal.info$N.allspmd,
+                       spmd.major = bal.info$spmd.major)
   load.balance(new.X.spmd, bal.info = rev.bal.info, comm = comm)
 } # End of unload.balance().
 
--- inst/doc/build_pdf.sh
+++ inst/doc/build_pdf.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-rm *.aux *.bbl *.blg *.log *.out *.toc
+rm *.aux *.bbl *.blg *.log *.out *.toc *.dvi
 pdflatex pbdDEMO-guide.Rnw
 bibtex pbdDEMO-guide
 pdflatex pbdDEMO-guide.Rnw
 pdflatex pbdDEMO-guide.Rnw
 pdflatex pbdDEMO-guide.Rnw
-rm *.aux *.bbl *.blg *.log *.out *.toc
+rm *.aux *.bbl *.blg *.log *.out *.toc *.dvi
+
+# Rscript -e 'tools::compactPDF("./pbdDEMO-guide.pdf",gs_quality="ebook")'
