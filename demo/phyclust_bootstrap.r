8613608ec46473db5100b40fc5035c9a68bf5520
snoweye <wccsnow@gmail.com>
2013-06-20 15:52:14 -0400

    update

--- demo/phyclust_bootstrap.r
+++ demo/phyclust_bootstrap.r
@@ -1,11 +1,12 @@
 library(pbdDEMO, quiet = TRUE)
 
-FUN <- funtion(jid){
+FUN <- function(jid){
+  Sys.sleep(1)
   jid * 10
 }
 
 ret <- task.pull(1:10, FUN)
 
-comm.print(ret, all.rank = TRUE)
+comm.print(ret)
 
 finalize()
