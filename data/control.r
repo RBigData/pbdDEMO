7695b1352d197604b778a08beec06e299f683d24
snoweye <wccsnow@gmail.com>
2013-08-06 16:33:39 -0400

    update

--- data/control.r
+++ data/control.r
@@ -2,6 +2,8 @@
 
 .DEMO.CT <- list(
   gbd.major = 1L,
-  divide.method = c("block.cyclic", "block0")
+  divide.method = c("block.cyclic", "block0"),
+  ictxt = 0L,
+  bldim = c(2L, 2L)
 ) # End of .DEMO.CT
 
