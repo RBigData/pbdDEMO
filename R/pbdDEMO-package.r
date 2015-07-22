2ddec57396ef86ffa0065eba759542c98ee3926a
wrathematics <wrathematics@gmail.com>
2015-07-22 16:08:50 -0400

    add package docs

---
+++ R/pbdDEMO-package.r
@@ -0,0 +1,17 @@
+#' Demonstrations and Examples for the pbd Project
+#' 
+#' Demos
+#' 
+#' \tabular{ll}{ Package: \tab pbdDMAC\cr Type: \tab Package\cr License: \tab
+#' GPL\cr LazyLoad: \tab yes\cr } This package requires an MPI library
+#' (OpenMPI, MPICH2, or LAM/MPI).
+#' 
+#' @name pbdDEMO-package
+#' @docType package
+#' @author Drew Schmidt \email{schmidt AT math.utk.edu}, Wei-Chen Chen, George
+#' Ostrouchov, and Pragneshkumar Patel.
+#' @references Programming with Big Data in R Website: \url{http://r-pbd.org/}
+#' @keywords Package
+NULL
+
+
